870e4bcc43d5ddc04f1cbee40e66bd7a7caa03d4
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-04-25 15:24:45 -0400

    Remove old audio player

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import GreetingContainer from './greetings/greeting_container';
 import SessionFormContainer from './sessions/session_form_container';
 import Modals from './modals/modals.jsx';
-import AudioPlayer from './main_components/audio/audio_player.jsx';
 import AudioPlayerNew from './main_components/audio/audio_player_new.jsx';
 import { Link } from 'react-router';
 
