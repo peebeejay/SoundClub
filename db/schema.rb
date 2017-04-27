59b31bdf68358f4dbe2221dcaf5dbc010e2c5f1d
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2017-04-27 17:08:03 -0400

    Modify seeds.rb; create likes table; install wavesurfer packages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426140400) do
+ActiveRecord::Schema.define(version: 20170427205906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20170426140400) do
   end
 
   add_index "follows", ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true, using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "song_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["song_id", "user_id"], name: "index_likes_on_song_id_and_user_id", unique: true, using: :btree
+  add_index "likes", ["song_id"], name: "index_likes_on_song_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "songs", force: :cascade do |t|
     t.string   "title",                           null: false
