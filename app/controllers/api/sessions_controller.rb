782159504e53c952727e1a7812cdb20283639870
Jay Puntham-Baker <jaypb@alphonso.tv>
2018-04-26 14:45:36 -0400

    minor change

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
-    params[:user][:username],
-    params[:user][:password]
+      params[:user][:username],
+      params[:user][:password]
     )
 
     if @user
